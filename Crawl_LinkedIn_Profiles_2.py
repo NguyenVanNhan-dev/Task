ee907e045e16fb8df6c5d58d9156e2651f76897d
Nguyễn Văn Nhân <118544931+NguyenVanNhan-dev@users.noreply.github.com>
2026-04-09 10:40:54 +0700

    Update Crawl_LinkedIn_Profiles_2.py

--- Crawl_LinkedIn_Profiles_2.py
+++ Crawl_LinkedIn_Profiles_2.py
@@ -35,6 +35,7 @@ def setup_driver():
     options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--lang=en-US")
     options.add_argument("--disable-blink-features=AutomationControlled")
